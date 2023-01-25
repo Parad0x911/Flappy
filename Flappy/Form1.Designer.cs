d0a6d80e2c57abd29cbee206e11e3130d8020594
Paradox911 <legoullon.g@gmail.com>
2023-01-25 16:17:20 +0100

    Add project files.

---
+++ Flappy/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿using System;
+
+namespace Flappy
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.P_3 = new System.Windows.Forms.PictureBox();
+            this.P_2 = new System.Windows.Forms.PictureBox();
+            this.P_1 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.P_3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.P_2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.P_1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // P_3
+            // 
+            this.P_3.Image = global::Flappy.Properties.Resources.Bird__1_;
+            this.P_3.Location = new System.Drawing.Point(569, 209);
+            this.P_3.Name = "P_3";
+            this.P_3.Size = new System.Drawing.Size(112, 112);
+            this.P_3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.P_3.TabIndex = 2;
+            this.P_3.TabStop = false;
+            this.P_3.Click += new System.EventHandler(this.P_3_Click);
+            // 
+            // P_2
+            // 
+            this.P_2.Image = global::Flappy.Properties.Resources.Bird__1_;
+            this.P_2.Location = new System.Drawing.Point(327, 66);
+            this.P_2.Name = "P_2";
+            this.P_2.Size = new System.Drawing.Size(112, 112);
+            this.P_2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.P_2.TabIndex = 1;
+            this.P_2.TabStop = false;
+            this.P_2.Click += new System.EventHandler(this.P_2_Click);
+            // 
+            // P_1
+            // 
+            this.P_1.Image = global::Flappy.Properties.Resources.Bird__1_;
+            this.P_1.Location = new System.Drawing.Point(87, 329);
+            this.P_1.Name = "P_1";
+            this.P_1.Size = new System.Drawing.Size(112, 112);
+            this.P_1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.P_1.TabIndex = 0;
+            this.P_1.TabStop = false;
+            this.P_1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 30;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick_1);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial Rounded MT Bold", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(296, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(209, 54);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Killed: 0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Highlight;
+            this.ClientSize = new System.Drawing.Size(782, 453);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.P_3);
+            this.Controls.Add(this.P_2);
+            this.Controls.Add(this.P_1);
+            this.Font = new System.Drawing.Font("Arial Rounded MT Bold", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.P_3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.P_2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.P_1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+       
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox P_1;
+        private System.Windows.Forms.PictureBox P_2;
+        private System.Windows.Forms.PictureBox P_3;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Flappy/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Flappy
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Bitmap bmp = new Bitmap(Properties.Resources.Aim);
+            Cursor crsr = new Cursor(bmp.GetHicon());
+            this.Cursor = crsr;
+        }
+
+        //Pour les tuers et ajouter au score
+        int kill;
+
+        //Pour faire voler les oiseaux
+        void Birds()
+        {
+            if(P_1.Left<0)
+            {
+                P_1.Left = 650;
+                P_1.Image = Properties.Resources.Bird__1_;
+            }
+            if(P_2.Left<0)
+            {
+                P_2.Left = 650;
+                P_2.Image = Properties.Resources.Bird__1_;
+            }
+            if(P_3.Left<0)
+            {
+                P_3.Left = 650;
+                P_3.Image = Properties.Resources.Bird__1_;
+            }
+            P_1.Left -= 5;
+            P_2.Left -= 5;
+            P_3.Left -= 5;
+        }
+        
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        //Piaf1
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            kill++;
+            label1.Text = "Killed : " + kill;
+            P_1.Image = Properties.Resources.Hit;
+        }
+
+        private void timer1_Tick_1(object sender, EventArgs e)
+        {
+            Birds();
+        }
+
+        //Ne marche pas
+        private void P_1_Click(object sender, EventArgs e)
+        {
+            kill++;
+            label1.Text = "Killed : " + kill;
+            P_1.Image = Properties.Resources.Hit;
+
+        }
+
+        //Piaf2
+        private void P_2_Click(object sender, EventArgs e)
+        {
+            kill++;
+            label1.Text = "Killed : " + kill;
+            P_2.Image = Properties.Resources.Hit;
+        }
+
+        //Piaf3
+        private void P_3_Click(object sender, EventArgs e)
+        {
+            kill++;
+            label1.Text = "Killed : " + kill;
+            P_3.Image = Properties.Resources.Hit;
+        }
+    }
+}
